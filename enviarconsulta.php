e73de7cbe79a70ac603ebd8fcd2f44c458793e30
tanomartin <tanomartin05@gmail.com>
2015-07-10 14:26:40 -0300

    agregado de logo y envio de mail

--- enviarconsulta.php
+++ enviarconsulta.php
@@ -1,27 +1,23 @@
 <?php
 
-	var_dump($_POST);
-	
 	// Casilla detino
-	//$casilla_destino = "info@gambetafemenina.com.ar";
 	$casilla_destino = "tanomartin05@gmail.com";
-	//$casilla_destino = "aialvarez@yahoo.com";
 	$nombre = (isset($_POST["nombre"]))? $_POST["nombre"] : "";
 	$email = (isset($_POST["email"]))? $_POST["email"] : "";
 	$telefono = (isset($_POST["telefono"]))? $_POST["telefono"] : "";
 	$mensaje = (isset($_POST["mensaje"]))? $_POST["mensaje"] : "";
 	
-	
+	$headers =  "MIME-Version: 1.0" . "\r\n"; 
+	$headers .= "From: ".$nombre."<".$email.">"."\r\n";
+	$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n"; 
 	$asunto = "Consulta - La Vistoria Trofeos";
-	$cuerpo_mail .= "Nombre y Apellido: ".$nombre."\r\n";
-	$cuerpo_mail .= "Email: ".$email."\r\n";
-	$cuerpo_mail .= "Telefono: ".$telefono."\r\n";
-	$cuerpo_mail .= "Mensaje: ".$mensaje."\r\n";
-	
-	print($cuerpo_mail);
+	$cuerpo_mail .= "Nombre y Apellido: ".$nombre."<br>";
+	$cuerpo_mail .= "Email: ".$email."<br>";
+	$cuerpo_mail .= "Telefono: ".$telefono."<br>";
+	$cuerpo_mail .= "Mensaje: ".$mensaje."<br>";
 
-	mail($casilla_destino, $asunto, $cuerpo_mail);
+	mail($casilla_destino, $asunto, $cuerpo_mail, $headers);
 	
-	//header('Location: contactoEnviado.html');
+	header('Location: contactoEnviado.html');
 	
 ?>
